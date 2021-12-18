7d9970b511587a4437110b5a47ea35761425acc2
h2o-Noob <arindamraina7743@gmail.com>
2021-12-18 13:15:47 +0530

    Added logic for binary converter

--- JS/binary.js
+++ JS/binary.js
@@ -1,14 +1,27 @@
-var base_I = document.getElementById("Base_I")
-var base_O = document.getElementById("Base_O")
-var from_input = document.getElementById("From")
-var to_input = document.getElementById("To")
+var base_I = document.getElementById("Base_I");
+var base_O = document.getElementById("Base_O");
+var from_input = document.getElementById("From");
+var to_input = document.getElementById("To");
 
-
-
-
-async function Convert(){
-    const url = `https://networkcalc.com/api/binary/${from_input.value}?from=${base_I.value}&to=${base_O.value}`
-    const response = await fetch(url)
-    const data = await response.json()
-    console.log(data)
+function Convert() {
+  if (base_I.value === base_O.value) {
+    to_input.value = from_input.value;
+  } else if (base_I.value === "10" && base_O.value === "2") {
+    let binaryNum = new Array(32);
+    let result = "";
+    n = from_input.value;
+    let i = 0;
+    while (n > 0) {
+      binaryNum[i] = n % 2;
+      n = Math.floor(n / 2);
+      i++;
+    }
+    for (let j = i - 1; j >= 0; j--) result += binaryNum[j];
+    to_input.value = result;
+    console.log(result)
+  } else {
+    var binary = from_input.value;
+    var digit = parseInt(binary, 2);
+    to_input.value = digit
+  }
 }
